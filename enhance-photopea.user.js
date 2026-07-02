86fa68f2f7c0fe7b46be70ed5323fb8a7ba23be1
zfrg <zfruiguo2010@163.com>
2026-07-02 20:05:07 +0800

    feat: 改用伪元素实现图标

--- enhance-photopea.user.js
+++ enhance-photopea.user.js
@@ -8,7 +8,7 @@
 // @icon         https://www.photopea.com/promo/thumb256.png
 // @updateURL    https://github.com/zfrg/userscript-enhance-photopea/raw/refs/heads/main/enhance-photopea.user.js
 // @downloadURL  https://github.com/zfrg/userscript-enhance-photopea/raw/refs/heads/main/enhance-photopea.user.js
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
@@ -17,6 +17,7 @@
     // Your code here...
     document.body.oncontextmenu = function() {return false;};
     // GM_addStyle(".app > div > div:nth-child(3) {height: 32px !important ; }");
+    GM_addStyle(".topbar::before{content:'';display:inline-block;width:18px;height:18px;background-image:url(https://www.photopea.com/promo/thumb256.png);background-size:cover;-webkit-transform:translate(5px,4px);-ms-transform:translate(5px,4px);transform:translate(5px,4px);-webkit-user-drag:none;app-region:no-drag;margin-right:10px}")
 
     setInterval(function() {
         var targetElement = document.querySelector('.app > div > div:nth-child(3)');
@@ -25,33 +26,4 @@
             targetElement.style.height = '32px';
         }
     }, 0);
-
-    var observer = new MutationObserver(function(mutationsList) {
-        var count = 0;
-
-        for (var mutation of mutationsList) {
-            if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-                var topbar = document.querySelector('.topbar');
-                if (topbar) {
-                    count++;
-
-                    if (count === 1) {
-                        var c = document.createElement('span');
-                        var icon = document.createElement('img');
-                        icon.src = "https://www.photopea.com/promo/thumb256.png";
-                        icon.style = "width: 18px;height: 18px;position: relative;left: 5px;top: 4px;bottom: 4px;padding-right: 10px;user-select: none;-webkit-user-select: none;-webkit-user-drag: none;app-region: no-drag;";
-                        icon.oncontextmenu = function() {
-                            return false;
-                        };
-                        c.appendChild(icon);
-                        topbar.insertBefore(c, topbar.firstChild);
-                        observer.disconnect();
-                    }
-                }
-            }
-        }
-    });
-
-    observer.observe(document.documentElement, { childList: true, subtree: true });
-
 })();
