8cff3c2bfe55383ae82e52fac0f95a51a1c3acba
zfrg <zfruiguo2010@163.com>
2026-07-16 20:15:11 +0800

    feat: 将图标设为可拖动区域，优化代码格式

--- enhance-photopea.user.js
+++ enhance-photopea.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Enhance Photopea
 // @namespace    https://github.com/zfrg/userscript-enhance-photopea
-// @version      1.0.4
+// @version      1.0.5
 // @description  优化 Photopea 使用体验
 // @author       zfrg
 // @match        https://www.photopea.com/*
@@ -16,8 +16,27 @@
 
     // Your code here...
     document.body.oncontextmenu = function() {return false;};
-    GM_addStyle(".topbar::before{content:'';display:inline-block;width:18px;height:18px;background-image:url(https://www.photopea.com/promo/thumb256.png);background-size:cover;-webkit-transform:translate(5px,4px);-ms-transform:translate(5px,4px);transform:translate(5px,4px);-webkit-user-drag:none;app-region:no-drag;margin-right:10px}")
-    GM_addStyle("div.flexrow.app:last-child > div:first-child > div:nth-child(3) > div:first-child{right:calc(100vw - env(titlebar-area-width, 100vw) - env(titlebar-area-x, 0px))!important;}")
+    GM_addStyle(`
+    .topbar::before {
+        content: '';
+        display: inline-block;
+        width: 18px;
+        height: 18px;
+        margin-right: 10px;
+        background-image: url(https://www.photopea.com/promo/thumb256.png);
+        background-size: cover;
+        -webkit-transform: translate(5px, 4px);
+        -ms-transform: translate(5px, 4px);
+        transform: translate(5px, 4px);
+        -webkit-user-drag: drag;
+        app-region: drag;
+    }
+    `)
+    GM_addStyle(`
+    div.flexrow.app:last-child > div:first-child > div:nth-child(3) > div:first-child {
+        right: calc(100vw - env(titlebar-area-width, 100vw) - env(titlebar-area-x, 0px)) !important;
+    }
+    `)
 
     setInterval(function() {
         var targetElement = document.querySelector('.app > div > div:nth-child(3)');
